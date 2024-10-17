b6967b96618d0a3e152d5f5991aeaf757d5669d2
unknown <maima.alive25@gmail.com>
2024-10-17 22:28:32 +0500

    my first commit

---
+++ function.js
@@ -0,0 +1,23 @@
+// function HalfFry(){
+//     let simpleFunc= 1+2+1
+//     console.log(simpleFunc)
+//     return simpleFunc
+// }
+// HalfFry()
+// let response= HalfFry()
+//     console.log(response)
+// 
+var month = 'June';
+var weather = 'cloudy';
+if (month === 'May' && weather === 'sunny') {
+    console.log('there is need of umbrella in day time');
+}
+if (month === 'June' && weather === 'rainy') {
+    console.log('there is a need of umbrella for day and night');
+}
+else if (month === 'June' && weather === 'cloudy') {
+    console.log('awesome weather');
+}
+else {
+    console.log('no need of umbrella');
+}
---
+++ function.ts
@@ -0,0 +1,20 @@
+// function HalfFry(){
+//     let simpleFunc= 1+2+1
+//     console.log(simpleFunc)
+//     return simpleFunc
+// }
+// HalfFry()
+// let response= HalfFry()
+//     console.log(response)
+
+    // another example
+
+    function data(age:number,rollNo:number){
+        return age+rollNo
+     }
+     console.log(data(25,69))
+    
+
+
+    
+    
